ee4d7c72bc4d6e36698d67360b477b5d78419b17
Aniket <aniketadhikari2@gmail.com>
2026-04-28 19:42:34 +0545

    Added component in a single page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { Layout } from './components/layout/Layout';
+import Hero from './components/ui/Hero';
 
+import About from './pages/About';
+import Skills from './pages/Skills';
+import { Projects } from './pages/Projects';
 
 
 function App() {
 
 
   return (
-    <h1 className='text-2xl'>Hello there</h1>
-  )
+    <div className="bg-[#030712] text-white">
+      <Layout>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+      </Layout>
+    </div>
+  );
 }
 
 export default App
